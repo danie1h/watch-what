d55b3b6c4d41cb2dfb49f9feead50f653bd587ef
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-11-30 09:53:57 -0800

    Add tmdb credits

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Layout from '../components/layout'
+import tmdbLogo from '../images/tmdb.png'
 
 const AboutPage = () => (
   <Layout>
@@ -9,6 +10,14 @@ const AboutPage = () => (
     </h1>
     <p>We're a group of individuals with the aspirations to...</p>
     <p>Our Mission....</p>
+    <img
+      src={tmdbLogo}
+      alt="Powered by The movie Database"
+      style={{ width: '128px' }}
+    />
+    <p>
+      This product uses the TMDb API but is not endorsed or certified by TMDb.
+    </p>
   </Layout>
 )
 
