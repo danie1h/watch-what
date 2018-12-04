e3c624920bde72511b84f0bfeae2a7ee5237b183
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-12-04 09:44:34 -0800

    Revise 404 page content

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,8 +3,13 @@ import Layout from '../components/layout'
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div style={{ minHeight: '81.5vh' }}>
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <p>
+        Refer to the links in the header about to navigate to a valid route.
+      </p>
+    </div>
   </Layout>
 )
 
