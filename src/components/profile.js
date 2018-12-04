80ac82ac0148639647826f2567d3e5659711b3ce
Daniel Humphreys <dhumphreys.co@gmail.com>
2018-12-04 13:48:34 -0800

    revise profile layout and style

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,31 +1,40 @@
 import React from 'react'
 
 const Profile = props => (
-  <div id="profile-container" style={{ margin: '1rem' }}>
+  <div
+    id="profile-container"
+    style={{
+      margin: '1rem',
+      backgroundColor: 'hsl(168, 59%, 97%)',
+      padding: '1rem',
+      borderRadius: '24px',
+      boxShadow: '1px 1px 4px hsl(168, 30%, 55%)',
+    }}
+  >
     <img
       src={props.profileImg}
       alt={props.personName + `'s profile`}
       style={{
+        display: 'block',
         width: '256px',
         borderRadius: '128px',
-        boxShadow: '1px 0 5px hsl(0,0%,30%)',
+        boxShadow: '1px 0 4px hsl(168, 20%, 10%)',
       }}
     />
     <div
       style={{
-        backgroundColor: 'hsl(0,0%,95%)',
-        color: 'hsl(217, 41%, 35%)',
+        color: 'hsl(217, 41%, 25%)',
         display: 'flex',
         width: '256px',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         padding: '.5rem 1rem',
-        borderRadius: '15px',
-        boxShadow: '0 5px 5px hsl(0,0%,50%)',
       }}
     >
-      <p style={{ margin: '.25rem 0' }}>{props.personName}</p>
+      <p style={{ margin: '.25rem 0', fontSize: '1.5em' }}>
+        <strong>{props.personName}</strong>
+      </p>
       <p
         style={{ margin: '.25rem 0', padding: '.25rem', textAlign: 'justify' }}
       >
